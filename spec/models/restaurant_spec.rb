6c447b9a5cc440c3366317cbaf83453d9ff295fd
Amin <amin.mattar@wework.com>
2019-08-04 15:36:48 +0300

    adding validation tests to model rspec

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,20 +1,44 @@
-# == Schema Information
-#
-# Table name: restaurants
-#
-#  id                :integer          not null, primary key
-#  name              :string
-#  address           :text
-#  accepts_10bis     :boolean
-#  max_delivery_time :integer
-#  created_at        :datetime         not null
-#  updated_at        :datetime         not null
-#  coordinates       :json
-#  cuisine_id        :integer
-#
-
 require 'rails_helper'
 
 RSpec.describe Restaurant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it 'ensure name is mandatory' do
+      cuisine = Cuisine.create(name: 'Persian')
+      restaurant = Restaurant.new(name: nil, address: 'address', max_delivery_time: 30, accepts_10bis: true, coordinates: {"lat": 11, "lng": 34.6}, cuisine: cuisine).save
+      expect(restaurant).to eq(false)
+    end
+    it { should validate_presence_of(:address) }
+    it { should validate_presence_of(:max_delivery_time) }
+    it { should validate_presence_of(:accepts_10bis) }
+    it { should validate_presence_of(:coordinates) }
+    
+    it 'ensure coordinates structure lng missing' do
+      cuisine = Cuisine.create(name: 'Persian')
+      restaurant = Restaurant.new(name: "test", address: 'address', max_delivery_time: 30, accepts_10bis: true, coordinates: {"lat": 11}, cuisine: cuisine).save
+      expect(restaurant).to eq(false)
+    end
+    it 'ensure coordinates structure lat missing' do
+      cuisine = Cuisine.create(name: 'Persian')
+      restaurant = Restaurant.new(name: "test", address: 'address', max_delivery_time: 30, accepts_10bis: true, coordinates: {"lng": 11}, cuisine: cuisine).save
+      expect(restaurant).to eq(false)
+    end
+    it 'ensure max deliveray time positive integer ' do
+      cuisine = Cuisine.create(name: 'Persian')
+      restaurant = Restaurant.new(name: "test", address: 'address', max_delivery_time: -1, accepts_10bis: true, coordinates: {"lat": 44, "lng": 11}, cuisine: cuisine).save
+      expect(restaurant).to eq(false)
+    end
+    it 'ensure address is mandatory' do
+      cuisine = Cuisine.create(name: 'Persian')
+      restaurant = Restaurant.new(name: "test", address: nil, max_delivery_time: 30, accepts_10bis: true, coordinates: {"lat": 11, "lng": 34.6}, cuisine: cuisine).save
+      expect(restaurant).to eq(false)
+    end
+    it { should belong_to(:cuisine) } 
+    it { should have_many(:reviews)}
+    it 'ensure restaurant save success' do
+      cuisine = Cuisine.create(name: 'italian')
+      restaurant = Restaurant.create(name: 'restaurant_dummy', address: 'address dummy', accepts_10bis: true, max_delivery_time: 30, coordinates: {"lat": 11, "lng": 78}, cuisine: cuisine)
+      expect(restaurant.errors).to be_empty
+    end
+
+  end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,18 +1,21 @@
-# == Schema Information
-#
-# Table name: reviews
-#
-#  id            :integer          not null, primary key
-#  name          :string
-#  rating        :integer
-#  comment       :text
-#  created_at    :datetime         not null
-#  updated_at    :datetime         not null
-#  restaurant_id :integer
-#
-
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validations' do
+    it 'ensure name is mandatory' do
+      review = Review.new(name: nil, rating: 3).save
+      expect(review).to eq(false)
+    end
+    it 'ensure rating between 1 and 5' do
+      review = Review.new(name: 'test_review', rating: 6).save
+      expect(review).to eq(false)
+    end
+    it 'ensure review save' do
+      cuisine = Cuisine.create(name: "italian")
+      restaurant = Restaurant.create(name: "restaurant_dummy", address: "address dummy", accepts_10bis: true, :max_delivery_time => 30, :coordinates => {"lat": 11, "lng": 78}, :cuisine => cuisine)
+      review = Review.new(name: 'test_review', rating: 5, restaurant: restaurant).save
+      expect(review).to eq(true)
+    end
+    it { should belong_to(:restaurant) } 
+  end
 end
