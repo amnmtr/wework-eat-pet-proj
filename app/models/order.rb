9e912da457ed586476a0040e000f195aabe9d49e
Amin <amin.mattar@wework.com>
2019-08-07 08:22:43 +0300

    adding Order model rspec

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: orders
+#
+#  id            :bigint           not null, primary key
+#  restaurant_id :bigint
+#  order_id      :string
+#  customer_name :string
+#  time          :datetime
+#  publish_time  :datetime
+#  status        :string
+#  info          :json
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#
+
 class Order < ApplicationRecord
     belongs_to :restaurant, foreign_key: :restaurant_id, optional: true
     validates_presence_of :order_id, :customer_name, :time, :publish_time, :status
